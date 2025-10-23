ea375a1fc2428d318e891007751a9e55cce2b50c
Mattchoman <mattcho35@gmail.com>
2025-10-22 20:40:53 -0700

    Extra bits, Full game
    Added the image and stuff to the game, should work. hopefully. maybe.
    Tweaked other files in minor ways, that shouldn't break anything. probably. maybe.

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame f = new JFrame("House Always Wins");
             f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            f.setSize(960, 540);
+            f.setSize(1980, 1080);
             f.setLocationRelativeTo(null);
 
             // Create a CardLayout and main panel
@@ -15,14 +17,14 @@ public class Main {
 
             // --- Create screens ---
             StartScreen start = new StartScreen(
-                e -> cards.show(container, "game"),
-                e -> cards.show(container, "settings"),
-                e -> cards.show(container, "credits")
+                new ActionListener() { public void actionPerformed(ActionEvent e) { cards.show(container, "game"); } },
+                new ActionListener() { public void actionPerformed(ActionEvent e) { cards.show(container, "settings"); } },
+                new ActionListener() { public void actionPerformed(ActionEvent e) { cards.show(container, "credits"); } }
             );
 
             GameClass game = new GameClass();
-            Settings settings = new Settings(e -> cards.show(container, "start"));
-            Credits credits = new Credits(e -> cards.show(container, "start"));
+            Settings settings = new Settings(new ActionListener() { public void actionPerformed(ActionEvent e) { cards.show(container, "start"); } });
+            Credits credits = new Credits(new ActionListener() { public void actionPerformed(ActionEvent e) { cards.show(container, "start"); } });
 
             // --- Add them to the card container ---
             container.add(start, "start");
